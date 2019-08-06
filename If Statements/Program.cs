5b2c2bf649807dd583afd87d2397b3d34b4c3032
oliviaalchemy <olivia@alchemytechs.com>
2019-08-06 16:20:25 +0100

    Add project files.

---
+++ If Statements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace If_Statements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;
+            bool isTall = false;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a tall male");
+            } else if (isMale && !isTall) {   
+                Console.WriteLine("You are a short male");
+            } else if (!isMale && isTall) {
+                Console.WriteLine("You are not male but you are tall");
+            } else
+            {
+                Console.WriteLine("You are not male and not tall");
+            }
+                 Console.ReadLine(); //Will execute: You are not male and you are not tall
+        }
+    }
+}
